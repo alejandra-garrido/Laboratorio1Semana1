80f8382437cd89ff91f75b42c6aa154b4e7d69b6
alejandra-garrido <agm287915@gmail.com>
2025-04-25 16:59:07 -0600

    main

--- src/laboratorio1semana1/Laboratorio1Semana1.java
+++ src/laboratorio1semana1/Laboratorio1Semana1.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package laboratorio1semana1;
 
@@ -9,12 +9,5 @@ package laboratorio1semana1;
  * @author aleg2
  */
 public class Laboratorio1Semana1 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
 }
